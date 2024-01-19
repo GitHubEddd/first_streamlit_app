4a6f00c3b6fa5c80892f8bb4bc3aced0c7d1d3d4
GitHubEddd <156943015+GitHubEddd@users.noreply.github.com>
2024-01-19 11:34:57 -0600

    Update streamlit_app.py
    I fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # write your own comment -what does the next line do?== La función json_normalize en Pandas se utiliza para aplanar datos JSON anidados en una tabla plana.
