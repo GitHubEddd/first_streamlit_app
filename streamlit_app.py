9641a0022f215e079e5b33b6fb0cce31afcf1a00
GitHubEddd <156943015+GitHubEddd@users.noreply.github.com>
2024-01-22 15:11:04 -0600

    Update streamlit_app.py
    I moved the Fruityvice code to a Try-Except (with a nested If-Else)

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,16 +24,23 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-#---------------->import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-
-# write your own comment -what does the next line do?== La función json_normalize en Pandas se utiliza para aplanar datos JSON anidados en una tabla plana.
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#############################################
+try
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit choise:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    #streamlit.write('The user entered ', fruit_choice)
+    #---------------->import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+    # write your own comment -what does the next line do?== La función json_normalize en Pandas se utiliza para aplanar datos JSON anidados en una tabla plana.
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+#############################################
 
 #don't run anything past here while we troubleshot:
 streamlit.stop()
