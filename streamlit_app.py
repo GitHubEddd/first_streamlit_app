252af706357595f25d4b1c5d694a6cedf933a486
GitHubEddd <156943015+GitHubEddd@users.noreply.github.com>
2024-01-22 15:50:08 -0600

    Update streamlit_app.py
    I created a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,22 +21,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
+#############################################
+#Create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-#############################################
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    #streamlit.write('The user entered ', fruit_choice)
-    #---------------->import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
-    #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-    # write your own comment -what does the next line do?== La función json_normalize en Pandas se utiliza para aplanar datos JSON anidados en una tabla plana.
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
+    back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
