51251a0af087fe5eb9e4f56c812150cb34919978
GitHubEddd <156943015+GitHubEddd@users.noreply.github.com>
2024-01-22 15:30:03 -0600

    Update streamlit_app.py
    I fixed another error

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,13 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 #############################################
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
+    streamlit.error("Please select a fruit to get information.")
   else:
     #streamlit.write('The user entered ', fruit_choice)
     #---------------->import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
     #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
     # write your own comment -what does the next line do?== La función json_normalize en Pandas se utiliza para aplanar datos JSON anidados en una tabla plana.
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
